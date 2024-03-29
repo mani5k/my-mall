adca6a0f13abb7658ca9e3f4419191b0e3c9c3f2
mani5k <45757717+mani5k@users.noreply.github.com>
2024-03-29 15:38:01 +0900

    update 1 file

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./reducer/store";
-// const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 ReactDOM.render(
   <Provider store={store}>
@@ -17,7 +18,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
